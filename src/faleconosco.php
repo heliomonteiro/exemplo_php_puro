85ff53d0dd184ea3196382fcec973a946dff6b62
Hélio Luiz Monteiro Júnior <heliojr.monteiro@yahoo.com.br>
2017-04-16 00:50:47 -0300

    composer - add namespace manual

--- src/faleconosco.php
+++ src/faleconosco.php
@@ -4,10 +4,10 @@
 $guzzle = new \Guzzle\Http\Client();
 $guzzle->getAllEvents();
 
-/*
+
 $client = new SON\Teste\Model\Client();
 $client->name = "Autoload PSR-4 está funcionando!";
 
-echo $client->name;*/
+echo $client->name;
 ?>
 Meu fale conosco
